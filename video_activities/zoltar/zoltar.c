62b713336eeb48ef7eaa2b1d2aab6dabb006c2d4
tylr <tylr@tylrs-MacBook-Pro.local>
2023-06-09 08:28:25 -0700

    adding zoltar vid activity

---
+++ video_activities/zoltar/zoltar.c
@@ -0,0 +1,22 @@
+/* Tyler Taormina - taormina@pdx.edu */
+/* Video Activity - zoltar */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/sockets.h>
+#include <unistd.h>
+
+#define BUF_SIZE 4096
+#define DEFAULT_PORT 10011
+#define FORTUNE_PATH "/usr/games/fortune"
+
+int
+main(int argc, char **argv)
+{
+
+    return EXIT_SUCCESS;
+}
